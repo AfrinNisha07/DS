43a45d0db4d0de4910ab70aaab572081b03800e6
AfrinNisha <mdafzalh431@gmail.com>
2025-10-08 22:35:44 +0530

    updating  this

---
+++ src/BubbleSort2.java
@@ -0,0 +1,18 @@
+public class BubbleSort2 {
+    public static void main(String[] args) {
+        int arr[]={0,5,3,0,52};
+        int a=arr.length;
+        for (int i = 0; i < a-1; i++) {
+            for (int j = 0; j <a-1-i ; j++) {
+                if (arr[j]==0&&arr[j+1]!=0){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int num:arr){
+            System.out.print(num+"  ");
+        }
+    }
+}
